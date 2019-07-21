5aba70354f00e7c61b51e87b7f8c21e5f9e5aa38
Dacio Romero <dacioromero@gmail.com>
2019-07-20 17:51:24 -0700

    Improve typing

--- src/clone.ts
+++ src/clone.ts
@@ -5,7 +5,7 @@ type UseRef = typeof useRef
 // > useRef() is basically useState({current: initialValue })[0]
 // https://twitter.com/dan_abramov/status/1099842565631819776
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const useFakeRef: UseRef = (initialValue?: any): any => {
+export const useFakeRef: UseRef = (initialValue?: unknown): ReturnType<UseRef> => {
   return useState({ current: initialValue })[0]
 }
 
--- src/contentEditable.ts
+++ src/contentEditable.ts
@@ -1,25 +1,40 @@
 import {
   useRef,
-  SyntheticEvent
+  SyntheticEvent,
+  Ref
 } from 'react'
 
 interface BasicElement { innerText: string }
 
-interface Config<T extends BasicElement> extends Record<PropertyKey, any> {
+interface Config<T extends BasicElement> {
   value: string;
   onChange(e: SyntheticEvent<T> & { target: { value: string } }): void;
 }
 
-// WIP
-export function useContentEditable<T extends BasicElement = any> ({ value, onChange, ...props }: Config<T>) {
-  const ref = useRef<T>()
+type SyntheticEventHandler<T> = (e: SyntheticEvent<T>) => void
 
-  if (ref.current !== undefined && value !== undefined) {
-    ref.current.innerText = value
+interface ContentEditable<T> {
+  ref: Ref<T | undefined>;
+  onInput: SyntheticEventHandler<T>;
+}
+
+// WIP
+export function useContentEditable<T extends BasicElement> ({
+  value,
+  onChange
+}: Config<T>): ContentEditable<T> {
+  const ref = useRef<T>(null)
+
+  const { current: elem } = ref
+
+  if (elem && value) {
+    elem.innerText = value
   }
 
-  function onInput (e: SyntheticEvent<T>) {
-    if (ref.current === undefined || onChange === undefined) {
+  const onInput: SyntheticEventHandler<T> = (e): void => {
+    const { current: elem } = ref
+
+    if (!elem || !onChange) {
       return
     }
 
@@ -27,14 +42,13 @@ export function useContentEditable<T extends BasicElement = any> ({ value, onCha
       ...e,
       target: {
         ...e.target,
-        value: ref.current.innerText
+        value: elem.innerText
       }
     })
   }
 
   return {
     ref,
-    onInput,
-    ...props
+    onInput
   }
 }
