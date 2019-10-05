50d377f0eb74d34cbca5c280ee014602968f9e42
Dacio Romero <dacioromero@gmail.com>
2019-10-05 01:25:29 -0700

    Make useDebounce behave more like lodash.debounce

--- packages/debounce/src/index.ts
+++ packages/debounce/src/index.ts
@@ -1,20 +1,61 @@
-import { useRef, useCallback } from 'react'
+import { useRef, useCallback, useEffect } from 'react'
 
-export function useDebounce<T extends (...args: unknown[]) => unknown>(
+function throwInvalidCall(): never {
+  throw new Error('Cannot call debounced function during first mount')
+}
+
+export interface Options {
+  leading?: boolean
+  trailing?: boolean
+  maxWait?: number
+}
+
+export function useDebounce<T extends (...args: any[]) => any>(
   func: T,
-  wait: number = 0
+  wait: number = 0,
+  { leading = false, trailing = true, maxWait }: Options = {}
 ): (...args: Parameters<T>) => void {
+  const funcRef = useRef<T | typeof throwInvalidCall>(throwInvalidCall)
   const timeoutRef = useRef<number>()
+  const delayStart = useRef<number | null>(null)
+
+  useEffect(() => {
+    funcRef.current = func
+  })
 
   return useCallback(
     (...args: Parameters<T>): void => {
       clearTimeout(timeoutRef.current)
 
-      // TODO: Figure out how to use normal setTimeout with window
-      timeoutRef.current = setTimeout(() => {
-        func(...args)
+      const isDelayed = !!delayStart.current
+      const now = Date.now()
+
+      if (isDelayed) {
+        if (
+          maxWait &&
+          delayStart.current &&
+          now - delayStart.current > maxWait
+        ) {
+          funcRef.current(...args)
+
+          delayStart.current = now
+        }
+      } else {
+        delayStart.current = now
+      }
+
+      if (leading && !isDelayed) {
+        funcRef.current(...args)
+      }
+
+      timeoutRef.current = setTimeout((): void => {
+        if (trailing) {
+          funcRef.current(...args)
+        }
+
+        delayStart.current = null
       }, wait)
     },
-    [func, wait]
+    [wait, leading, trailing, maxWait]
   )
 }
